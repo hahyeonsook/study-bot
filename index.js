a6b015c424f088ff105d34e3bfb05cc9f98c857a
jajaspider <jaja_spider@naver.com>
2022-04-17 02:17:12 +0900

    feat: Add interactionCreate
    interactionCreate에 대한 이벤트를 추가하였습니다.
    
    comment : 해당 이벤트에서는 로직을 수용하는게아니라 이벤트 요청에따른 로그를 남기거나, validation처리를 담당하고, router로 던지는 구조만 포함하였으면 좋겠습니다. router에서는 수신하여, 각각의 router에 보내주는 역할을 했으면 좋겠습니다.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const {
+    adminRouter,
+    commonRouter
+} = require('./src/routers');
+
 const discordClient = require("./src/core/discord");
+
+discordClient.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const {
+        member: {
+            roles
+        }
+    } = interaction;
+    let isAdmin = false;
+    roles.cache.map(role => {
+        if (role.name == '관리자') {
+            isAdmin = true
+        }
+    });
+
+    let router;
+    if (isAdmin) {
+        router = new adminRouter(interaction);
+    } else {
+        router = new commonRouter(interaction);
+    }
+    router.init();
+});
